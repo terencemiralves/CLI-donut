1f36f804d762fa56385da8bdaa7ca2d8b0e42f61
terence miralves <terence.miralves@gmail.com>
2024-08-01 14:18:01 +0200

    feat fork: added a fork that checks the input and allows the change of size in the printed message

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <termios.h>
+#include <sys/types.h>
 
 
 
 struct termios oldt;
 
 //The main issue with input reading in C is that it always wait for the user to press enter, boring and non usefull for us. So I found out a way to remove the need for the enter press and here it is
-void set_nonblocking_input() {
+void set_nonblocking_input() 
+{
     struct termios newt;
 
     // Get current terminal attributes
@@ -27,17 +29,63 @@ void reset_terminal()
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 }
 
-int main() {
-    char c;
-    set_nonblocking_input();
-    while (1)
+int main() 
+{
+    int pipedfd[2];
+    pid_t pid;
+    int size = 5;
+
+    if (pipe(pipedfd) == -1)
     {
-        printf("Press any key to continue...\n");
-        c = getchar();
-        printf("\nYou pressed: %c\n", c);
-        if (c == 'c')
-            break;
+        perror("pipe");
+        return 1;
     }
+
+    set_nonblocking_input();
+
+    pid = fork();
+
+    if (pid == -1)
+    {
+        perror("fork");
+        return 1;
+    }
+
+    if (pid == 0)
+    {
+        //in the child read the inputs
+        close(pipedfd[0]);
+        while (1)
+        {
+            char c = getchar();
+            if (c == 'c')
+            {
+                size = -1;
+                write(pipedfd[1], &size, sizeof(size));
+                close(pipedfd[1]);
+            }
+            else if (c == 'l' || c == 'j')
+            {
+                size += c == 'l' ? 1 : -1;
+                write(pipedfd[1], &size, sizeof(size));
+            }
+        }
+    }
+    else 
+    {
+        // parent
+        while (1)
+        {
+            printf("Press j or l to change the size and c to close\n");
+            for (int i = 0; i < size; i++)
+                printf("-");
+            printf("\n");
+            read(pipedfd[0], &size, sizeof(size));
+            if (size == -1)
+                break;
+        } 
+    }
+
     reset_terminal();
     return 0;
 }
